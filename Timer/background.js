e54796c3c2d6ab0811187949392c25e98f270a61
Daniel Florencio <danflorencio94@gmail.com>
2017-01-05 19:55:19 -0800

    Getting ready to put on the chrome webstore
    Added comments, fixed a few bugs, preparing to upload to chrome webstore.

--- Timer/background.js
+++ Timer/background.js
@@ -1,19 +1,28 @@
 {
   var currDomain;   // The curent domain user is visitng
+
+  // Using parallel arrays, efficient on space, but lookup not so great,
+  // maybe switch to hash table in next version?
   var sites = [];   // The array of the websites the user has visited
   var timers = [];  // The array of the actual times corresponding to the sites
   var startTime;    // To measure elapsed time -> endTime - startTime
-  var endTime;
+  var endTime;      // When switching to a new tab or loading different domain
   var realTime;     // To perform an update when popup is clicked on
 
+  // logSites(domain) will search through the sites array looking for the domain
+  // passed in, if the domain is in the sites array, there is a previous value
+  // in the corresponding timers array and we simply increment within the
+  // calling function. Otherwise, the site has not been visited yet, and we
+  // append the new domain to the sites array and a corresponding "dummy" value
+  // into the timers array.
   function logSites(domain) {
     var visited = false;
     // Easier way to do this, indexOf()?
     for (var i = 0; i < sites.length; i++) {
       if (sites[i] == domain) {
         // If we know a website has been visited perviously, it is already
-        // in the sites array and has a corresponding timer, don't mess with
-        // anything
+        // in the sites array and has a corresponding timer, simply increment
+        // within the calling function
         visited = true;
         break;
       }
@@ -23,11 +32,12 @@
       // Add a domain to the list of visited sites
       sites.push(domain);
       // Add a corresponding dummy value to the timers array
-      timers.push(0);
+      timers.push(1 - 0);
     }
-    //console.log(domain);
   }
 
+  // getDomainIndex(domain) simply searches through the sites array checking to
+  // see if the domain passed is in the sites array.
   function getDomainIndex(domain) {
     for (var i = 0; i < sites.length; i++) {
       if (sites[i] == domain) {
@@ -38,35 +48,49 @@
     return 0;
   }
 
+  // formatTime(millisecs) is used to format x amount of milliseconds passed in
+  // into a common "Hour : Minute: Second" string. Only called when formatting
+  // the global timer.
   function formatTime(millisecs) {
-    // Javascript is weird.
     var second = Math.floor((millisecs / 1000) % 60);
     var minute = Math.floor((millisecs / (1000 * 60)) % 60);
     var hour = Math.floor((millisecs / (1000 * 60 * 60)) % 24);
 
     var time = hour.toString() + "h: " + minute.toString() + "m: " + second.toString() + "s";
+
     return time;
   }
 
+  // realTimeUpdate is used when first starting the extension. Check if there is
+  // a corresponding value in the timers array with the current domain, if there
+  // is, increment the value at the corresponding index in the timers array.
+  // If there is no corresponding value in the timers array, push 0 milliseconds
+  // into the corresponding index in the timers array.
   function realTimeUpdate() {
     realTime = new Date();
-    var index = getDomainIndex(currDomain);
-    timers[index] += realTime - startTime;
+
+    console.log(getDomainIndex(currDomain));
+    console.log(timers[getDomainIndex(currDomain)]);
+    if (timers[getDomainIndex(currDomain)] == null) {
+      console.log("Error: no corresponding value in timers array");
+      // There is no corresponding value in the timers array, start off
+      // with 0 milliseconds
+      timers[getDomainIndex(currDomain)] = 0;
+      startTime = new Date();
+      return;
+    }
+
+    timers[getDomainIndex(currDomain)] += realTime - startTime;
     startTime = new Date();
   }
 
-  // Never fired, only starts when chrome starts, not when extension starts
-  chrome.runtime.onStartup.addListener(function(info){
-    console.log("background.js chrome.runtime.onStartup.addListener");
-  })
-
-  // This will execute when the extension is installed
+  // This function will execute when the extension is installed
   chrome.runtime.onInstalled.addListener(function(info){
     //console.log("background.js chrome.runtime.onInstalled.addListener");
-    timedText();
+    debug();
     // Measure the elasped time
     startTime = new Date();
-    // Push the website we are currently visitng!
+    // Push the website we are currently visitng
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = new URL(tabs[0].url);
         currDomain = url.hostname;
@@ -75,7 +99,7 @@
     });
   })
 
-  // This will execute when tabs are switched
+  // This function will execute when tabs are changed.
   chrome.tabs.onActivated.addListener(function(info) {
     //console.log("switched tab!");
     // When we switch a tab, are we on the same domain still?
@@ -102,8 +126,11 @@
     });
   });
 
-  // When you're loading up a page, wait for it to complete and
-  // then start the timer for it.
+  // This function will execute when loading a page, wait for it to complete and
+  // then start the timer for the domain. For example, if a user types into
+  // the address bar, "facebook.com" and tries to access that page, the function
+  // will wait until facebook is loaded before measuring time spent visiting
+  // facebook.
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
       chrome.tabs.getSelected(null, function (tab1) {
@@ -127,6 +154,7 @@
   var clear;
   var count = 0;
 
+  // Keep the stopwatch going and increment every second.
   var stopWatch = function() {
     clear = setTimeout("stopWatch()", 1000);
   }
@@ -142,6 +170,9 @@
   var getHours;
   var totalTime = document.getElementById('status');
 
+  // startWatch() is used for the global timer, it keeps track of the seconds
+  // and icrements the global timer at every second, formatting the milliseconds
+  // into a string that can be displayed to popup.html
   function startWatch() {
     // Check if seconds == 60, if so increment minutes and set seconds to 0
     if (seconds == 60) {
@@ -164,10 +195,12 @@
     clearTime = setTimeout(startWatch, 1000);
   }
 
-  function timedText() {
+  // Function exists only to debug.
+  function debug() {
     setTimeout(myTimeout3, 20000)
   }
 
+  // Function exists only to debug.
   function myTimeout3() {
     console.log("20 seconds");
     for (var i = 0; i < sites.length; i++) {
--- Timer/popup.js
+++ Timer/popup.js
@@ -1,37 +1,37 @@
-/**
- * Create a timer for the active tab.
- *
+/*
+ * popup.js is the code that is executed when the user clicks on the popup
+ * within Chrome to start the timer.
  */
-function startTimer() {
-  // Create a new timer for the url the user is on
-  var myTimer = new chrome.Interval();
-  document.getElementById('status').textContent = "Awesome";
-}
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-
-})
-
-chrome.runtime.onInstalled.addListener(function(details){
-
-})
-
-chrome.runtime.onStartup.addListener(function(details){
-
-})
-
+// window.onload() is invoked whenever the user clicks on the
+// popup within Chrome
 window.onload = function() {
+  // bgPage is background.js and contains all code necessary to run in
+  // the background.
   var bgPage = chrome.extension.getBackgroundPage();
+  // Debug statement
   console.log("bgPage.startTime()");
+  // This counter exists because we don't want to start
+  // a global clock incrementing more than once, if we didn't have this counter
+  // and the user clicked the popup twice, the global clock would run at
+  // twice the speed. If clicked three times, three times the speed, etc.
   if (bgPage.count == 0) {
     bgPage.startWatch();
     bgPage.count++;
   }
+
+  // loopy() exists to refresh the global clock when the popup is activated, if
+  // we didn't update the clock every second, the only time the global clock
+  // would be accurate is right at the instant the popup is clicked
   var resetMe;
-  function loopy() {
+  function refresh() {
     document.getElementById('status').innerHTML = chrome.extension.getBackgroundPage().totalTime;
     resetMe = setTimeout(loopy, 1000);
   }
+
+  // addTimes() will add the domains and the times a user has spent visiting
+  // them to the popup within Chrome, to acheive this we will create an
+  // unordered list and append list elements to them
   function addTimes() {
     for (var i = 0; i < bgPage.sites.length; i++) {
       var node = document.createElement("LI");
@@ -41,7 +41,7 @@ window.onload = function() {
       document.getElementById("list").appendChild(node);
     }
   }
-  loopy();
-  addTimes();
+  refresh();
   bgPage.realTimeUpdate();
+  addTimes();
 }
