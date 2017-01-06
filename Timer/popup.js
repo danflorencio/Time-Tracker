91ce877214516460d3b1525f4461303fbc533100
Daniel Florencio <danflorencio94@gmail.com>
2017-01-05 20:04:22 -0800

    Preparing to add to the Chrome Web Store
    Added comments, made code prettier, getting ready to put this onto the Chrome Web Store.

--- Timer/popup.js
+++ Timer/popup.js
@@ -1,47 +1,49 @@
 /*
- * popup.js is the code that is executed when the user clicks on the popup
- * within Chrome to start the timer.
+ * popup.js is executed when the popup.html is clicked on. The api used to
+ * grab trending headlines and put them into a conveinient JSON format is
+ * provided by newsapi.org, and the news outlet choice is The Washington Post.
  */
 
-// window.onload() is invoked whenever the user clicks on the
-// popup within Chrome
+var theNewScript = document.createElement("script");
+theNewScript.type = "text/javascript";
+theNewScript.src = "jquery.js";
+
 window.onload = function() {
-  // bgPage is background.js and contains all code necessary to run in
-  // the background.
-  var bgPage = chrome.extension.getBackgroundPage();
-  // Debug statement
-  console.log("bgPage.startTime()");
-  // This counter exists because we don't want to start
-  // a global clock incrementing more than once, if we didn't have this counter
-  // and the user clicked the popup twice, the global clock would run at
-  // twice the speed. If clicked three times, three times the speed, etc.
-  if (bgPage.count == 0) {
-    bgPage.startWatch();
-    bgPage.count++;
-  }
+  // Debug
+  console.log("window.onload");
 
-  // loopy() exists to refresh the global clock when the popup is activated, if
-  // we didn't update the clock every second, the only time the global clock
-  // would be accurate is right at the instant the popup is clicked
-  var resetMe;
-  function refresh() {
-    document.getElementById('status').innerHTML = chrome.extension.getBackgroundPage().totalTime;
-    resetMe = setTimeout(loopy, 1000);
-  }
+  // Grab the metadata and put it into JSON
+  $.get("https://newsapi.org/v1/articles?source=the-washington-post&sortBy=top&apiKey=hidden", function(newsy) {
 
-  // addTimes() will add the domains and the times a user has spent visiting
-  // them to the popup within Chrome, to acheive this we will create an
-  // unordered list and append list elements to them
-  function addTimes() {
-    for (var i = 0; i < bgPage.sites.length; i++) {
-      var node = document.createElement("LI");
-      var textNode = document.createTextNode(bgPage.sites[i] + "  "
-                                             + bgPage.formatTime(bgPage.timers[i]));
-      node.appendChild(textNode);
-      document.getElementById("list").appendChild(node);
+    // Begin building popup.html
+    for (var i = 0; i < newsy.articles.length; i++) {
+      // Create a link with the article's title
+      var link = document.createElement('a');
+      link.textContent = newsy.articles[i].title;
+      link.href = newsy.articles[i].url;
+
+      // Grab the image and set the attributes
+      var newImg = document.createElement("IMG");
+      newImg.src = newsy.articles[i].urlToImage;
+      newImg.title = newsy.articles[i].description;
+
+      // Now append to the HTML body
+      document.body.appendChild(link);
+      document.body.appendChild(newImg);
     }
-  }
-  refresh();
-  bgPage.realTimeUpdate();
-  addTimes();
+
+    // Give credit to newsAPI.org
+    var newLink = document.createElement('p');
+    newLink.href = 'https://newsAPI.org';
+    newLink.innerHTML = 'Powered by NewsAPI.org';
+    document.body.appendChild(newLink);
+  });
 }
+
+// Make those links in popup.html clickable so that a user can open them in
+// a new tab
+window.addEventListener('click', function(e){
+  if(e.target.href !== undefined){
+    chrome.tabs.create({url:e.target.href})
+  }
+})
